29d9000249c489e8cf8e49477a433f7803bfd81a
Víctor Escribano <v.escsan@gmail.com>
2025-03-13 15:48:28 +0100

    feat: the structure of cities has been expanded with new properties.

--- database/migrations/0000_03_07_190508_create_cities_table.php
+++ database/migrations/0000_03_07_190508_create_cities_table.php
@@ -26,17 +26,16 @@ class CreateCitiesTable extends Migration
 
         Schema::create($this->tableName, function (Blueprint $table) {
             $table->id();
-            $table->foreignId('country_id')->constrained(config()->string('atlas.countries_tablename'));
-
-            if (config()->boolean('atlas.entities.states')) {
-                $table->foreignId('state_id')->constrained(config()->string('atlas.states_tablename'));
-            }
-
             $table->string('name');
+            $table->foreignId('state_id')->constrained(config()->string('atlas.states_tablename'));
+            $table->string('state_code', 5);
+            $table->string('state_name');
+            $table->foreignId('country_id')->constrained(config()->string('atlas.countries_tablename'));
             $table->string('country_code', 3);
-            $table->string('state_code', 5)->nullable();
-            $table->string('latitude')->nullable();
-            $table->string('longitude')->nullable();
+            $table->string('country_name');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('wiki_data_id')->nullable();
 
             $table->index(['country_id', 'state_id']);
         });
